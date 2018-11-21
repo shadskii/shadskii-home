7429c40bdcba6be901362ac8f2bd5d3d33873f1f
Jake Hassel <jake.k.hassel@gmail.com>
2018-11-21 18:06:53 -0500

    Adding pwa support for ios

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   baseUrl: '/',
+  pwa: {
+    name: 'Shadskii',
+    themeColor: '#1e88e5',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black-translucent',
+  },
 };
